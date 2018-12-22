7197b0927c89ba94fc9e90b848b22e89b7f10a32
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 15:08:04 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ using namespace std;
 
 int main()
 {
-  int n;
+  int n,counting;
   cout<<"Creating a selection sort object:\n";
   SelectionSort ss;
   cout<<"Creating an unsorted array for sorting with selection sort:\n";
     int arr[8] = {14,33,27,10,35,19,48,44};
   cout<<"Inserted array is:\n";
-  for(int index=0;i<sizeof(arr);i++){
+  for(counting=0;i<sizeof(arr);i++){
     cout<<arr[i]<<endl;
   }
     n = sizeof(arr)/sizeof(arr[0]);
     ss.SelectionSorter(arr,n);
-  
+  cout<<"After sorting the array is now:\n";
+  for(counting=0;i<sizeof(arr);i++){
+    cout<<arr[i]<<endl;
+  }
   
