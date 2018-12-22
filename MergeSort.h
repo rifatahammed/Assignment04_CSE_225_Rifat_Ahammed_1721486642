4e12f032744e64468406fbf5e7f57de6303aa585
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 14:51:56 +0600

    Create MergeSort.h

---
+++ MergeSort.h
@@ -0,0 +1,9 @@
+#ifndef MERGESORT_H_INCLUDED
+#define MERGESORT_H_INCLUDED
+
+class MergeSort
+{
+public:
+    void merge_sort(int value[],int startIndex, int endIndex);
+};
+#endif // MERGESORT_H_INCLUDED
