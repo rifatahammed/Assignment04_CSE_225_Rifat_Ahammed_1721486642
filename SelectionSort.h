f825af2198c791ae933bbd0e818f44de7c5d0c00
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 14:56:43 +0600

    Create SelectionSort.h

---
+++ SelectionSort.h
@@ -0,0 +1,8 @@
+#ifndef SELECTIONSORT_H_INCLUDED
+#define SELECTIONSORT_H_INCLUDED
+class SelectionSort
+{
+public:
+    void SelectionSorter(int arr[], int arr_size);
+};
+#endif // SELECTIONSORT_H_INCLUDED
