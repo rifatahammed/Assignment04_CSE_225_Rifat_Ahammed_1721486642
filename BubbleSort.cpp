3306aaada88b2a590bb28cdb3be8fdccbe6b9a2f
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 14:43:01 +0600

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,1 +1,32 @@
+#include "BubbleSort.h"
+#include <iostream>
+using namespace std;
 
+void BubbleSort:: BubbleSorter(int *arr, int n)
+{
+  
+    bool swapped = true;
+    int index = 0;
+    int temp;
+    while (swapped)
+    {
+        swapped = false;
+        index++;
+        for (int index2 = 0; index2 < n - index; ++index2)
+        {
+            if (arr[index2] > arr[index2 + 1])
+            {
+                temp = arr[index2];
+                arr[index2] = arr[index2 + 1];
+                arr[index2 + 1] = temp;
+                swapped = true;
+                for(int index3 = 0; index3 < n; ++index3)
+                {
+                    cout << arr[index3]<<" ";
+                }
+                cout <<"\n";
+            }
+
+        }
+    }
+}
