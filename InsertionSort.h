4e92e773c5b45cc6233713fb85cbfe5be279c235
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 14:49:24 +0600

    Create InsertionSort.h

---
+++ InsertionSort.h
@@ -0,0 +1,8 @@
+#ifndef INSERTIONSORT_H_INCLUDED
+#define INSERTIONSORT_H_INCLUDED
+class InsertionSort
+{
+public:
+    void InsertionSorter(int arr[], int arr_size);
+};
+#endif // INSERTIONSORT_H_INCLUDED
