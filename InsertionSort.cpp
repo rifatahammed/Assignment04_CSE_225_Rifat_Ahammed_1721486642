e724844440c8314f85449124a031d137bd47ef04
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-22 14:48:21 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,1 +1,29 @@
+#include "InsertionSort.h"
+#include <iostream>
+using namespace std;
 
+void InsertionSort::InsertionSorter(int arr[], int arr_size)
+{
+
+    if(arr_size > 1)
+    {
+        int size = arr_size;
+        for(int index = 1; index < size; ++index)
+        {
+            int index2 = index - 1;
+            int key = arr[index];
+            while(index2 >= 0 && arr[index2] > key)
+            {
+                arr[index2+1] = arr[index2];
+                --index2;
+            }
+            arr[index2+1] = key;
+            for(int index3 = 0; index3 < arr_size; ++index3)
+    {
+        cout << arr[index3]<<" ";
+    }
+    cout <<"\n";
+        }
+
+    }
+}
